96e96f958958b52b502dc3e822fd970d33c4df72
heyile <2513931335@qq.com>
2018-12-27 22:37:38 +0800

    完善account service

--- cse-account-service/src/main/java/com/huawei/account/cseaccountservice/controller/AccountController.java
+++ cse-account-service/src/main/java/com/huawei/account/cseaccountservice/controller/AccountController.java
@@ -6,14 +6,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.huawei.account.cseaccountservice.entity.TAccount;
 import com.huawei.account.cseaccountservice.service.AccountService;
 
 @Controller
-@RequestMapping("/test")
+@RequestMapping("/account")
 public class AccountController {
   private final static Logger logger = LoggerFactory.getLogger(AccountService.class);
 
@@ -21,13 +27,42 @@ public class AccountController {
   private AccountService accountService;
 
   @ResponseBody
-  @RequestMapping("/getAll")
+  @GetMapping("/getAllAccount")
   public List<TAccount> getAllAccount() {
-    List<TAccount> tAccounts = accountService.findAllAccounts();
-
-    if (tAccounts != null && tAccounts.size() == 0) {
-      return null;
+    List<TAccount> allAccounts = accountService.getAllAccounts();
+    if (allAccounts == null || allAccounts.size() == 0) {
+      logger.warn("accounts list is empty");
     }
-    return tAccounts;
+    return allAccounts;
+  }
+
+  @ResponseBody
+  @PostMapping(value = "/create")
+  public String createAccount(@RequestBody TAccount account) {
+    return accountService.insertAccount(account);
+  }
+
+  @ResponseBody
+  @DeleteMapping(value = "/delete/{id}")
+  public void createAccount(@PathVariable(value = "id") String id) {
+    accountService.deleteAccountById(id);
+  }
+
+  @ResponseBody
+  @GetMapping(value = "/search/{id}")
+  public TAccount searchAccount(@PathVariable(value = "id") String id) {
+    return accountService.findAccountById(id);
+  }
+
+  @ResponseBody
+  @GetMapping(value = "save")
+  public double saveMoney(@RequestParam("money") double money, @RequestParam("id") String id) {
+    return accountService.saveMoney(id, money);
+  }
+
+  @ResponseBody
+  @GetMapping(value = "consume")
+  public double consume(@RequestParam("money") double money, @RequestParam("id") String id) {
+    return accountService.consume(id, money);
   }
 }
--- cse-account-service/src/main/java/com/huawei/account/cseaccountservice/service/AccountService.java
+++ cse-account-service/src/main/java/com/huawei/account/cseaccountservice/service/AccountService.java
@@ -1,5 +1,6 @@
 package com.huawei.account.cseaccountservice.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -18,11 +19,65 @@ public class AccountService {
   @Autowired
   private TAccountMapper accountMapper;
 
-  public List<TAccount> findAllAccounts() {
+  public String insertAccount(TAccount tAccount) {
+    int insert = accountMapper.insert(tAccount);
+    logger.info("result : " + insert);
+    logger.info("id : " + tAccount.getId());
+    return tAccount.getId();
+  }
+
+  public List<TAccount> getAllAccounts() {
     List<TAccount> tAccounts = accountMapper.selectAll();
-    if (tAccounts != null && tAccounts.size() == 0) {
-      return null;
+    if (tAccounts != null && tAccounts.size() != 0) {
+      return tAccounts;
     }
-    return tAccounts;
+    logger.warn("No Account in the list");
+    return new ArrayList<>();
+  }
+
+  public TAccount findAccountById(String id) {
+    TAccount tAccount = accountMapper.selectByPrimaryKey(id);
+    if (tAccount != null) {
+      return tAccount;
+    }
+    logger.warn("the Account {} is not exist ", id);
+    return null;
+  }
+
+  public int deleteAccountById(String id) {
+    int i = accountMapper.deleteByPrimaryKey(id);
+    if (i == 0) {
+      logger.warn("the Account {} may not exist ", id);
+    }
+    return i;
+  }
+
+  public double saveMoney(String id, Double money) {
+    TAccount tAccount = accountMapper.selectByPrimaryKey(id);
+    if (tAccount == null) {
+      logger.warn("the Account {} may not exist ", id);
+      return 0;
+    }
+    double allMoney = money + tAccount.getRestMoney();
+    updateMoney(id, allMoney);
+    return allMoney;
+  }
+
+  public double consume(String id, Double money) {
+    TAccount tAccount = accountMapper.selectByPrimaryKey(id);
+    if (tAccount == null) {
+      logger.warn("the Account {} may not exist ", id);
+      return 0;
+    }
+    double allMoney = tAccount.getRestMoney() - money;
+    updateMoney(id, allMoney);
+    return allMoney;
+  }
+
+  private void updateMoney(String id, Double money) {
+    int i = accountMapper.updateMoneyById(id, money);
+    if (i == 0) {
+      logger.warn("the Account {} may not exist ", id);
+    }
   }
 }
